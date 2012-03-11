4d0564e2bd141f63d79d5ca2a0a6dba906516337
Craig Campbell <iamcraigcampbell@gmail.com>
2012-03-11 17:08:16 -0400

    Add CSS support

---
+++ js/language/css.js
@@ -0,0 +1,43 @@
+window.Rainbow = window.Rainbow || {};
+
+Rainbow.extend('css', [
+    {
+        'name': 'constant.hex-color',
+        'pattern': /#([a-fA-F0-9]{3}|[a-fA-F0-9]{6})(?=;|\s)/g
+    },
+    {
+        'matches': {
+            1: 'integer',
+            2: 'keyword.px'
+        },
+        'pattern': /(\d+)(px)/g
+    },
+    {
+        'name': 'string',
+        'pattern': /('|")(.*?)\1/g
+    },
+    {
+        'name': 'support.css-property',
+        'pattern': /[\w-]+(?=\s|:)(?!.*\{)/g
+    },
+    {
+        'name': 'meta.tag',
+        'pattern': /[\w]+(?=.*\{)/g
+    },
+    {
+        'name': 'meta.class',
+        'pattern': /\.[\w]+(?=.*\{)/g
+    },
+    {
+        'name': 'meta.id',
+        'pattern': /\#[\w]+(?=.*\{)/g
+    },
+    {
+        'matches': {
+            2: 'support.vender-prefix',
+            3: 'support.value'
+        },
+        'pattern': /(:|,)\s?(-o-|-moz-|-webkit-|-ms-)?([a-zA-Z-]*)(?=\b)(?!.*\{)/g
+    }
+], true);
+
