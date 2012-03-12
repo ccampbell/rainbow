0d738c47d6a70a5911c5ec6323098784fb7dc76d
Craig Campbell <iamcraigcampbell@gmail.com>
2012-03-12 00:54:35 -0400

    Build script updates
    - Add ability to create a package of all languages
    - Add a comment with version number and languages to output

--- util/compile.py
+++ util/compile.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import sys, os, subprocess
+import sys, os, subprocess, glob
 
 sys.argv.pop(0)
 languages = sys.argv
@@ -9,6 +9,14 @@ js_path = os.path.dirname(__file__) + '/../js/'
 
 js_files_to_include = [js_path + 'rainbow.js']
 included_languages = []
+
+all_files = '--all' in sys.argv
+
+if all_files:
+    languages = []
+    files = glob.glob(js_path + 'language/*.js')
+    for file in files:
+        languages.append(os.path.basename(file).replace('.js', ''))
 
 for language in languages:
     path = js_path + 'language/' + language + '.js'
@@ -23,7 +31,26 @@ print 'waiting for closure compiler...'
 proc = subprocess.Popen(['java', '-jar', '/usr/local/compiler-latest/compiler.jar', '--compilation_level', 'ADVANCED_OPTIMIZATIONS'] + js_files_to_include, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
 output, err = proc.communicate()
 
-file_name = 'rainbow' + ('+' + '+'.join(included_languages) if len(included_languages) else '') + '.min.js'
+file_name = 'rainbow' + ('-custom' if len(included_languages) or all_files else '') + '.min.js'
+
+lines = output.splitlines()
+comments = lines[0:4]
+version = comments[1].replace(' @version ', '')
+url = comments[2].replace(' @url ', '')
+new_comment = '/* Rainbow v' + version + ' ' + url
+
+if all_files or len(included_languages):
+    new_comment += ' | included languages: ' + ', '.join(included_languages)
+
+new_comment += ' */'
+
+output = new_comment + '\n' + '\n'.join(lines[4:])
+
+print "\nincluded:"
+for file in js_files_to_include:
+    print "    ",os.path.basename(file)
+print ""
+
 print 'writing to file:',file_name
 
 new_file = js_path + file_name
@@ -31,5 +58,3 @@ new_file = js_path + file_name
 file = open(new_file, "w")
 file.write(output)
 file.close()
-
-
