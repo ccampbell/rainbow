7ca2fe574e049b42ea4a1825394d74e976bcef54
Craig Campbell <iamcraigcampbell@gmail.com>
2012-03-29 02:51:05 -0400

    Start adding tests

--- tests/language/html-test.js
+++ tests/language/html-test.js
@@ -1,4 +1,7 @@
 RainbowTester.startTest('html');
 
+RainbowTester.run('comment', '<!-- this is a comment -->', '<span class="comment html">&lt;!-- this is a comment --&gt;</span>');
+RainbowTester.run('multiline-comment', '<!-- this is a comment\non two lines -->', '<span class="comment html">&lt;!-- this is a comment\non two lines --&gt;</span>');
+RainbowTester.run('paragraph', '<p class="test">this is a paragraph</p>', '<span class="support tag"><span class="support tag">&lt;</span><span class="support tag-name">p</span></span> <span class="support attribute">class</span><span class="support operator">=</span><span class="string quote">"</span><span class="string value">test</span><span class="string quote">"</span><span class="support tag close">&gt;</span>this is a paragraph<span class="support tag"><span class="support tag">&lt;</span><span class="support tag special">/</span><span class="support tag-name">p</span></span><span class="support tag close">&gt;</span>');
 
 RainbowTester.endTest('html');
--- tests/tests.js
+++ tests/tests.js
@@ -1,15 +1,12 @@
 window.RainbowTester = (function() {
+    var _language,
+        results = {};
+
     function _addScript(url) {
 
         var script = document.createElement('script');
         script.src = url + '?' + new Date().getTime();
         document.getElementsByTagName('body')[0].appendChild(script);
-    }
-
-    function _runTest(languages, i) {
-        if (i < languages.length) {
-
-        }
     }
 
     function _runTests(e) {
@@ -25,12 +22,35 @@ window.RainbowTester = (function() {
             _addScript(RAINBOW_PATH + '/language/' + this.value + '.js');
         });
 
-        _runTest(languages, 0);
         for (i = 0; i < languages.length; ++i) {
             _addScript('language/' + languages[i] + '-test.js');
         }
 
         // load in the actual tests
+    }
+
+    function _initResults() {
+        if (!results[_language]) {
+            results[_language] = {};
+        }
+    }
+
+    function _pass(test_name) {
+        _initResults();
+        results[_language][test_name] = {
+            success: true
+        };
+        console.log(test_name, 'PASS');
+    }
+
+    function _fail(test_name, expected, actual) {
+        _initResults();
+        results[_language][test_name] = {
+            success: false,
+            expected: expected,
+            actual: actual
+        };
+        console.warn(test_name, 'FAIL', 'EXPECTED', expected, 'ACTUAL', actual);
     }
 
     return {
@@ -40,10 +60,21 @@ window.RainbowTester = (function() {
 
         startTest: function(name) {
             console.log('starting', name);
+            _language = name;
         },
 
         endTest: function(name) {
             console.log('ending', name);
+            _language = null;
+        },
+
+        run: function(test_name, code, expected) {
+            Rainbow.color(code, _language, function(actual) {
+                if (expected == actual) {
+                    return _pass(test_name);
+                }
+                return _fail(test_name, expected, actual);
+            });
         }
     };
 }) ();
