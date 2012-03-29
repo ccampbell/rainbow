009b41d030cfc79fb7037c729881c6de900b0ca1
Craig Campbell <iamcraigcampbell@gmail.com>
2012-03-29 00:38:10 -0400

    Increase versions

--- util/builder.py
+++ util/builder.py
@@ -13,10 +13,10 @@ class RainbowBuilder(object):
         'css': '1.0.1',
         'generic': '1.0.1',
         'html': '1.0',
-        'javascript': '1.0.1',
+        'javascript': '1.0.2',
         'php': '1.0',
         'python': '1.0',
-        'ruby': '1.0.1',
+        'ruby': '1.0.2',
         'shell': '1.0.2'
     }
 
