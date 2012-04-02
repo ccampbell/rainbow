3f78997909067da164d7c38cdf1c40c75ee4640b
Craig Campbell <iamcraigcampbell@gmail.com>
2012-04-01 22:34:17 -0400

    Increase version in builder

--- util/builder.py
+++ util/builder.py
@@ -13,7 +13,7 @@ class RainbowBuilder(object):
     def __init__(self, js_path, closure_path, theme_path=None):
 
         self.versions = {
-            'c': '1.0.1',
+            'c': '1.0.2',
             'css': '1.0.6',
             'generic': '1.0.6',
             'html': '1.0.4',
