3f93a35ecb162377330f4d95ac4ee0f2b6ff6407
Craig Campbell <iamcraigcampbell@gmail.com>
2012-06-23 11:14:32 -0400

    Update formatting

--- js/language/smalltalk.js
+++ js/language/smalltalk.js
@@ -5,38 +5,48 @@
  * @version 1.0
  */
 Rainbow.extend('smalltalk', [
-    {'name': 'keyword.pseudovariable',
-     'pattern': /self|thisContext/g
-    },
-    {'name': 'keyword.constant',
-     'pattern': /false|nil|true/g
-    },
-    {'name': 'string',
-     'pattern': /'([^']|'')*'/g
-    },
-    {'name': 'string.symbol',
-     'pattern': /#\w+|#'([^']|'')*'/g
-    },
-    {'name': 'string.character',
-     'pattern': /\$\w+/g
+    {
+        'name': 'keyword.pseudovariable',
+        'pattern': /self|thisContext/g
     },
     {
-     'name': 'comment',
-     "pattern": /"([^"]|"")*"/g
+        'name': 'keyword.constant',
+        'pattern': /false|nil|true/g
     },
-    {'name': 'constant.numeric',
-     'pattern': /-?\d+(\.\d+)?((r-?|s)[A-Za-z0-9]+|e-?[0-9]+)?/g
+    {
+        'name': 'string',
+        'pattern': /'([^']|'')*'/g
     },
-    {'name': 'entity.name.class',
-     'pattern': /\b[A-Z]\w*/g
+    {
+        'name': 'string.symbol',
+        'pattern': /#\w+|#'([^']|'')*'/g
     },
-    {'name': 'entity.name.function',
-     'pattern': /\b[a-z]\w*:?/g
+    {
+        'name': 'string.character',
+        'pattern': /\$\w+/g
     },
-    {'name': 'entity.name.binary',
-     'pattern': /(&lt;|&gt;|&amp;|[=~\|\\\/!@*\-_+])+/g
+    {
+        'name': 'comment',
+        'pattern': /"([^"]|"")*"/g
     },
-    {'name': 'operator.delimiter',
-     'pattern': /;[\(\)\[\]\{\}]|#\[|#\(^\./g
+    {
+        'name': 'constant.numeric',
+        'pattern': /-?\d+(\.\d+)?((r-?|s)[A-Za-z0-9]+|e-?[0-9]+)?/g
+    },
+    {
+        'name': 'entity.name.class',
+        'pattern': /\b[A-Z]\w*/g
+    },
+    {
+        'name': 'entity.name.function',
+        'pattern': /\b[a-z]\w*:?/g
+    },
+    {
+        'name': 'entity.name.binary',
+        'pattern': /(&lt;|&gt;|&amp;|[=~\|\\\/!@*\-_+])+/g
+    },
+    {
+        'name': 'operator.delimiter',
+        'pattern': /;[\(\)\[\]\{\}]|#\[|#\(^\./g
     }
 ], true);
