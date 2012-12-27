92f53a483ba4072cbb49336239abcc784bce7eba
Bruno Dias <contact@diasbruno.com.br>
2012-12-27 14:48:14 -0300

    added 'trait' keyword.

--- js/language/php.js
+++ js/language/php.js
@@ -76,7 +76,7 @@ Rainbow.extend('php', [
             4: 'storage.modifier.extends',
             5: 'entity.other.inherited-class'
         },
-        'pattern': /\b(abstract|final)?\s?(class)\s(\w+)(\sextends\s)?([\w\\]*)?\s?\{?(\n|\})/g
+        'pattern': /\b(abstract|final)?\s?(class|trait)\s(\w+)(\sextends\s)?([\w\\]*)?\s?\{?(\n|\})/g
     },
     {
         'name': 'keyword.static',
