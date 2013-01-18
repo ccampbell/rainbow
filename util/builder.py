3bb414c6e90b67abc8f5d2083d20cd508c9f10f7
Craig Campbell <iamcraigcampbell@gmail.com>
2013-01-18 18:43:27 -0500

    Fix issue with runtime functions in javascript.  Fixes #95

--- util/builder.py
+++ util/builder.py
@@ -22,7 +22,7 @@ class RainbowBuilder(object):
             'haskell': '1.0.1',
             'html': '1.0.7',
             'java': '1.0',
-            'javascript': '1.0.7',
+            'javascript': '1.0.8',
             'lua': '1.0',
             'php': '1.0.7',
             'python': '1.0.7',
