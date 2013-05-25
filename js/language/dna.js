c1b576e3a46bc38040b145df8a2afdae7d217af5
Panagiotis Tigkas <ptigas@gmail.com>
2013-05-25 20:55:27 +0100

    added DNA highlighting support

---
+++ js/language/dna.js
@@ -0,0 +1,24 @@
+/**
+ * dna
+ *
+ * @author Panagiotis Tigas
+ * @version 1.0.0
+ */
+Rainbow.extend('dna', [
+    {
+        'name': 'entity',
+        'pattern': /a|A/gm
+    },
+    {
+        'name': 'storage',
+        'pattern': /c|C/gm
+    },
+    {
+        'name': 'constant.hex-color',
+        'pattern': /g|G/gm
+    },
+    {
+        'name': 'support',
+        'pattern': /t|T/gm
+    },
+], true);
