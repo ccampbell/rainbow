96fab8225927583d41fe21bad5f7a4dac006a21b
Craig Campbell <iamcraigcampbell@gmail.com>
2014-10-06 21:21:15 -0400

    Move all themes into SASS

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,6 +15,21 @@ module.exports = function(grunt) {
             }
         },
 
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: [{
+                    expand: true,
+                    src: ['themes/sass/*.sass'],
+                    dest: 'themes/css',
+                    flatten: true,
+                    ext: '.css'
+                }]
+            }
+        },
+
         complexity: {
             options: {
                 errorsOnly: false,
@@ -32,6 +47,7 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-complexity');
     grunt.loadNpmTasks('grunt-mocha');
+    grunt.loadNpmTasks('grunt-contrib-sass');
 
     grunt.registerTask('default', [
         'complexity',
