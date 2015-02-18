5ea5888852ac71e9c964d3d14b89151938e875bf
Tako Schotanus <tako@codejive.org>
2015-02-19 01:09:15 +0800

    ceylon: clean up

--- src/language/ceylon.js
+++ src/language/ceylon.js
@@ -1,14 +1,10 @@
 /**
-* Java patterns
+* Ceylon patterns
 *
-* @author Leo Accend
+* @author Leo Accend, Gavin King
 * @version 1.0.0
 */
 Rainbow.extend( "ceylon", [
-  /*{
-    name: "constant",
-    pattern: /\b(false|null|true|[A-Z_]+)\b/g
-  },*/
   {
     matches: {
       1: "keyword",
@@ -17,7 +13,6 @@ Rainbow.extend( "ceylon", [
     pattern: /(import|module|package)(\s)*((\w|\.)+)/g
   },
   {
-    // see http://docs.oracle.com/javase/tutorial/java/nutsandbolts/_keywords.html
     name: "keyword",
     pattern: /\b(assembly|module|package|import|alias|class|interface|object|given|value|assign|void|function|new|of|extends|satisfies|abstracts|in|out|return|break|continue|throw|assert|dynamic|if|else|switch|case|for|while|try|catch|finally|then|let|this|outer|super|is|exists|nonempty|while)\b/g
   },
