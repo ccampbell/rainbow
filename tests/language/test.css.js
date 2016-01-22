cb1534185178bdd1430c72de794ea38eae21643f
Fabio Di Monte <fdimonte@expedia.com>
2016-01-22 18:02:02 +0100

    enhanced css code highlight

--- tests/language/test.css.js
+++ tests/language/test.css.js
@@ -33,7 +33,7 @@ describe(language, function() {
 
         'margin:10px 20px 5px 30px;',
 
-        '<span class="support css-property">margin</span>:<span class="constant numeric">10</span><span class="keyword unit">px</span> <span class="constant numeric">20</span><span class="keyword unit">px</span> <span class="constant numeric">5</span><span class="keyword unit">px</span> <span class="constant numeric">30</span><span class="keyword unit">px</span>;'
+        '<span class="css-property">margin</span>:<span class="constant numeric">10<span class="keyword unit absolute">px</span></span> <span class="constant numeric">20<span class="keyword unit absolute">px</span></span> <span class="constant numeric">5<span class="keyword unit absolute">px</span></span> <span class="constant numeric">30<span class="keyword unit absolute">px</span></span>;'
     );
 
     run(
@@ -43,7 +43,7 @@ describe(language, function() {
 
         'margin: 1cm 2cm 1.3cm 4cm;',
 
-        '<span class="support css-property">margin</span>: <span class="constant numeric">1</span><span class="keyword unit">cm</span> <span class="constant numeric">2</span><span class="keyword unit">cm</span> <span class="constant numeric">1</span>.<span class="constant numeric">3</span><span class="keyword unit">cm</span> <span class="constant numeric">4</span><span class="keyword unit">cm</span>;'
+        '<span class="css-property">margin</span>: <span class="constant numeric">1<span class="keyword unit absolute">cm</span></span> <span class="constant numeric">2<span class="keyword unit absolute">cm</span></span> <span class="constant numeric">1.3<span class="keyword unit absolute">cm</span></span> <span class="constant numeric">4<span class="keyword unit absolute">cm</span></span>;'
     );
 
     run(
@@ -53,7 +53,7 @@ describe(language, function() {
 
         'font-size: 1.2em;',
 
-        '<span class="support css-property">font-size</span>: <span class="constant numeric">1</span>.<span class="constant numeric">2</span><span class="keyword unit">em</span>;'
+        '<span class="css-property">font-size</span>: <span class="constant numeric">1.2<span class="keyword unit relative">em</span></span>;'
     );
 
     run(
@@ -64,8 +64,8 @@ describe(language, function() {
         'width: 100%\n' +
         'height: 100%',
 
-        '<span class="support css-property">width</span>: <span class="constant numeric">100</span><span class="keyword unit">%</span>\n' +
-        '<span class="support css-property">height</span>: <span class="constant numeric">100</span><span class="keyword unit">%</span>'
+        '<span class="css-property">width</span>: <span class="constant numeric">100<span class="keyword unit percentage">%</span></span>\n' +
+        '<span class="css-property">height</span>: <span class="constant numeric">100<span class="keyword unit percentage">%</span></span>'
     );
 
     run(
@@ -100,11 +100,11 @@ describe(language, function() {
             '   transition: color .8s ease-in;\n' +
         '}',
 
-        '<span class="entity name tag">code</span> <span class="entity name tag">span</span> {\n' +
-        '   <span class="support css-property"><span class="support vendor-prefix">-moz-</span>transition</span>: <span class="support css-value">color</span> .<span class="constant numeric">8</span><span class="keyword unit">s</span> ease-in;\n' +
-        '   <span class="support css-property"><span class="support vendor-prefix">-o-</span>transition</span>: <span class="support css-value">color</span> .<span class="constant numeric">8</span><span class="keyword unit">s</span> ease-in;\n' +
-        '   <span class="support css-property"><span class="support vendor-prefix">-webkit-</span>transition</span>: <span class="support css-value">color</span> .<span class="constant numeric">8</span><span class="keyword unit">s</span> ease-in;\n' +
-        '   <span class="support css-property">transition</span>: <span class="support css-value">color</span> .<span class="constant numeric">8</span><span class="keyword unit">s</span> ease-in;\n' +
+        '<span class="selector"><span class="entity name tag">code</span> <span class="entity name tag">span</span> </span>{\n' +
+        '   <span class="css-property"><span class="vendor-prefix">-moz-</span>transition</span>: <span class="support text-value">color</span> <span class="constant numeric">.8<span class="keyword unit time">s</span></span> <span class="support text-value">ease-in</span>;\n' +
+        '   <span class="css-property"><span class="vendor-prefix">-o-</span>transition</span>: <span class="support text-value">color</span> <span class="constant numeric">.8<span class="keyword unit time">s</span></span> <span class="support text-value">ease-in</span>;\n' +
+        '   <span class="css-property"><span class="vendor-prefix">-webkit-</span>transition</span>: <span class="support text-value">color</span> <span class="constant numeric">.8<span class="keyword unit time">s</span></span> <span class="support text-value">ease-in</span>;\n' +
+        '   <span class="css-property">transition</span>: <span class="support text-value">color</span> <span class="constant numeric">.8<span class="keyword unit time">s</span></span> <span class="support text-value">ease-in</span>;\n' +
         '}'
     );
 
@@ -115,7 +115,7 @@ describe(language, function() {
 
         'p {',
 
-        '<span class="entity name tag">p</span> {'
+        '<span class="selector"><span class="entity name tag">p</span> </span>{'
     );
 
     run(
@@ -125,7 +125,7 @@ describe(language, function() {
 
         'p.intro {',
 
-        '<span class="entity name tag">p</span><span class="entity name class">.intro</span> {'
+        '<span class="selector"><span class="entity name tag">p</span><span class="entity name class">.intro</span> </span>{'
     );
 
     run(
@@ -135,7 +135,7 @@ describe(language, function() {
 
         'p#intro {',
 
-        '<span class="entity name tag">p</span><span class="entity name id">#intro</span> {'
+        '<span class="selector"><span class="entity name tag">p</span><span class="entity name id">#intro</span> </span>{'
     );
 
     run(
@@ -145,29 +145,7 @@ describe(language, function() {
 
         'p > span {',
 
-        '<span class="entity name tag">p</span> <span class="direct-descendant">&gt;</span> <span class="entity name tag">span</span> {'
-    );
-
-    run(
-        language,
-
-        'scss',
-
-        'article {\n' +
-        '   &amp;.cool {\n' +
-        '       p {\n' +
-        '           margin-top: 20px;\n' +
-        '       }\n' +
-        '   }\n' +
-        '}',
-
-        '<span class="entity name tag">article</span> {\n' +
-        '   <span class="entity name sass">&amp;</span><span class="entity name class">.cool</span> {\n' +
-        '       <span class="entity name tag">p</span> {\n' +
-        '           <span class="support css-property">margin-top</span>: <span class="constant numeric">20</span><span class="keyword unit">px</span>;\n' +
-        '       }\n' +
-        '   }\n' +
-        '}'
+        '<span class="selector"><span class="entity name tag">p</span><span class="direct-child"> &gt; </span><span class="entity name tag">span</span> </span>{'
     );
 
     run(
@@ -177,7 +155,7 @@ describe(language, function() {
 
         'p { color: #fff; margin-top: 10px; }',
 
-        '<span class="entity name tag">p</span> { <span class="support css-property">color</span>: <span class="constant hex-color">#fff</span>; <span class="support css-property">margin-top</span>: <span class="constant numeric">10</span><span class="keyword unit">px</span>; }'
+        '<span class="selector"><span class="entity name tag">p</span> </span>{ <span class="css-property">color</span>: <span class="constant hex-color">#fff</span>; <span class="css-property">margin-top</span>: <span class="constant numeric">10<span class="keyword unit absolute">px</span></span>; }'
     );
 
     run(
@@ -192,11 +170,11 @@ describe(language, function() {
         '    background:         linear-gradient(#f7f7f7, #e8e8e8);\n' +
         '}',
 
-        '<span class="entity name class">.gradient</span> {\n' +
-        '    <span class="support css-property">background</span>: <span class="support vendor-prefix">-webkit-</span><span class="support css-value">linear-gradient</span>(<span class="constant hex-color">#f7f7f7</span>, <span class="constant hex-color">#e8e8e8</span>);\n' +
-        '    <span class="support css-property">background</span>:    <span class="support vendor-prefix">-moz-</span><span class="support css-value">linear-gradient</span>(<span class="constant hex-color">#f7f7f7</span>, <span class="constant hex-color">#e8e8e8</span>);\n' +
-        '    <span class="support css-property">background</span>:      <span class="support vendor-prefix">-o-</span><span class="support css-value">linear-gradient</span>(<span class="constant hex-color">#f7f7f7</span>, <span class="constant hex-color">#e8e8e8</span>);\n' +
-        '    <span class="support css-property">background</span>:         <span class="support css-value">linear-gradient</span>(<span class="constant hex-color">#f7f7f7</span>, <span class="constant hex-color">#e8e8e8</span>);\n' +
+        '<span class="selector"><span class="entity name class">.gradient</span> </span>{\n' +
+        '    <span class="css-property">background</span>: <span class="css-method"><span class="method-name"><span class="vendor-prefix">-webkit-</span>linear-gradient</span><span class="method-params">(<span class="constant hex-color">#f7f7f7</span>, <span class="constant hex-color">#e8e8e8</span>)</span></span>;\n' +
+        '    <span class="css-property">background</span>:    <span class="css-method"><span class="method-name"><span class="vendor-prefix">-moz-</span>linear-gradient</span><span class="method-params">(<span class="constant hex-color">#f7f7f7</span>, <span class="constant hex-color">#e8e8e8</span>)</span></span>;\n' +
+        '    <span class="css-property">background</span>:      <span class="css-method"><span class="method-name"><span class="vendor-prefix">-o-</span>linear-gradient</span><span class="method-params">(<span class="constant hex-color">#f7f7f7</span>, <span class="constant hex-color">#e8e8e8</span>)</span></span>;\n' +
+        '    <span class="css-property">background</span>:         <span class="css-method"><span class="method-name">linear-gradient</span><span class="method-params">(<span class="constant hex-color">#f7f7f7</span>, <span class="constant hex-color">#e8e8e8</span>)</span></span>;\n' +
         '}'
     );
 
@@ -212,11 +190,80 @@ describe(language, function() {
         '   font-weight: bold;\n' +
         '}',
 
-        '<span class="entity name class">.maps-headline</span>,\n' +
+        '<span class="selector"><span class="entity name class">.maps-headline</span>,\n' +
         '<span class="entity name class">.maps-subline</span>,\n' +
         '<span class="entity name class">.chart-headline</span>,\n' +
-        '<span class="entity name class">.chart-subline</span> {\n' +
-        '   <span class="support css-property">font-weight</span>: <span class="support css-value">bold</span>;\n' +
+        '<span class="entity name class">.chart-subline</span> </span>{\n' +
+        '   <span class="css-property">font-weight</span>: <span class="support text-value">bold</span>;\n' +
         '}'
     );
+
+    /**
+     * IE HACKS
+     */
+    
+    run(
+        language,
+        
+        'hack for ie6 only',
+        
+        '_color: blue;',
+        
+        '<span class="hack"><span class="ie-6">_</span></span><span class="css-property">color</span>: <span class="support text-value">blue</span>;'
+    );
+    
+    run(
+        language,
+        
+        'hack for ie6 and ie7 (aka lte7)',
+        
+        '*color: blue;\n'+
+        '#color: blue;\n'+
+        'color: blue !ie;',
+        
+        '<span class="hack"><span class="ie-lte7">*</span></span><span class="css-property">color</span>: <span class="support text-value">blue</span>;\n'+
+        '<span class="hack"><span class="ie-lte7">#</span></span><span class="css-property">color</span>: <span class="support text-value">blue</span>;\n'+
+        '<span class="css-property">color</span>: <span class="support text-value">blue</span> <span class="hack"><span class="ie-lte7">!ie</span></span>;'
+    );
+    
+    run(
+        language,
+        
+        'hack for ie7+ (aka gt6)',
+        
+        'color/**/: blue;',
+        
+        '<span class="css-property">color</span><span class="hack"><span class="ie-gt6">/**/</span></span>: <span class="support text-value">blue</span>;'
+    );
+    
+    run(
+        language,
+        
+        'hack for ie7 and ie8',
+        
+        'color/*\\**/: blue\\9;',
+        
+        '<span class="css-property">color</span><span class="hack"><span class="ie-gt6">/*\\**/</span></span>: <span class="support text-value">blue</span><span class="hack"><span class="ie-lte9">\\9</span></span>;'
+    );
+    
+    run(
+        language,
+        
+        'hack for ie6+ (aka lte9)',
+        
+        'color: blue\\9;',
+        
+        '<span class="css-property">color</span>: <span class="support text-value">blue</span><span class="hack"><span class="ie-lte9">\\9</span></span>;'
+    );
+    
+    run(
+        language,
+        
+        'hack for ie8 and ie9',
+        
+        'color: blue\\0/;',
+        
+        '<span class="css-property">color</span>: <span class="support text-value">blue</span><span class="hack"><span class="ie-8-9">\\0/</span></span>;'
+    );
+
 });
