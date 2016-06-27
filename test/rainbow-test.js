73551bd256ff96e5aa6ae62b07c23df81b129d6b
Craig Campbell <iamcraigcampbell@gmail.com>
2016-06-26 22:32:08 -0400

    Add unit test for applying global class

--- test/rainbow-test.js
+++ test/rainbow-test.js
@@ -1,3 +1,4 @@
+/* global Rainbow */
 
 /////////////////////////
 // Helpers and globals //
@@ -9,7 +10,7 @@ const expect = chai.expect;
 // Test suite //
 ////////////////
 
-describe('Rainbow', function() {
+describe('Rainbow', () => {
     it('Basic things are defined', () => {
         expect(Rainbow).to.exist;
         expect(Rainbow.color).to.be.a('function');
@@ -17,5 +18,19 @@ describe('Rainbow', function() {
         expect(Rainbow.onHighlight).to.be.a('function');
         expect(Rainbow.addClass).to.be.a('function');
         expect(Rainbow.addAlias).to.be.a('function');
+    });
+
+    it('Should apply global class', (done) => {
+        Rainbow.extend([{
+            name: 'name',
+            pattern: /Craig/gm
+        }]);
+
+        Rainbow.addClass('global');
+
+        Rainbow.color('My name is Craig', 'generic', function(result) {
+            expect(result).to.equal('My name is <span class="name global">Craig</span>');
+            done();
+        });
     });
 });
