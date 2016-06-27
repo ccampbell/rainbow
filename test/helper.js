de8568d10a98163ea504c65e7f0ad439c5b1f350
Craig Campbell <iamcraigcampbell@gmail.com>
2016-06-26 23:24:39 -0400

    Add ability to run only a single test for debugging

--- test/helper.js
+++ test/helper.js
@@ -5,8 +5,13 @@
 
 const expect = chai.expect;
 
-export function run(lang, description, code, result) {
-    it(description, (done) => {
+export function run(lang, description, code, result, only = false) {
+    let toCall = it;
+    if (only) {
+        toCall = it.only;
+    }
+
+    toCall(description, (done) => {
         Rainbow.color(code, lang, (html) => {
             expect(result).to.equal(html);
             done();
