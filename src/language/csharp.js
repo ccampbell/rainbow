29823376f60f813d9093d68faf1362938f2d1d27
Craig Campbell <iamcraigcampbell@gmail.com>
2016-07-03 13:38:54 -0400

    Update indentation to use spaces instead of tabs

--- src/language/csharp.js
+++ src/language/csharp.js
@@ -5,17 +5,17 @@
 * @version 1.0.1
 */
 Rainbow.extend('csharp', [
-	{
+    {
         // @see http://msdn.microsoft.com/en-us/library/23954zh5.aspx
-		name: 'constant',
-		pattern: /\b(false|null|true)\b/g
-	},
-	{
-		// @see http://msdn.microsoft.com/en-us/library/x53a06bb%28v=vs.100%29.aspx
-		// Does not support putting an @ in front of a keyword which makes it not a keyword anymore.
-		name: 'keyword',
-		pattern: /\b(abstract|add|alias|ascending|as|async|await|base|bool|break|byte|case|catch|char|checked|class|const|continue|decimal|default|delegate|descending|double|do|dynamic|else|enum|event|explicit|extern|false|finally|fixed|float|foreach|for|from|get|global|goto|group|if|implicit|int|interface|internal|into|in|is|join|let|lock|long|namespace|new|object|operator|orderby|out|override|params|partial|private|protected|public|readonly|ref|remove|return|sbyte|sealed|select|set|short|sizeof|stackalloc|static|string|struct|switch|this|throw|try|typeof|uint|unchecked|ulong|unsafe|ushort|using|value|var|virtual|void|volatile|where|while|yield)\b/g
-	},
+        name: 'constant',
+        pattern: /\b(false|null|true)\b/g
+    },
+    {
+        // @see http://msdn.microsoft.com/en-us/library/x53a06bb%28v=vs.100%29.aspx
+        // Does not support putting an @ in front of a keyword which makes it not a keyword anymore.
+        name: 'keyword',
+        pattern: /\b(abstract|add|alias|ascending|as|async|await|base|bool|break|byte|case|catch|char|checked|class|const|continue|decimal|default|delegate|descending|double|do|dynamic|else|enum|event|explicit|extern|false|finally|fixed|float|foreach|for|from|get|global|goto|group|if|implicit|int|interface|internal|into|in|is|join|let|lock|long|namespace|new|object|operator|orderby|out|override|params|partial|private|protected|public|readonly|ref|remove|return|sbyte|sealed|select|set|short|sizeof|stackalloc|static|string|struct|switch|this|throw|try|typeof|uint|unchecked|ulong|unsafe|ushort|using|value|var|virtual|void|volatile|where|while|yield)\b/g
+    },
     {
         matches: {
             1: 'keyword',
@@ -53,7 +53,7 @@ Rainbow.extend('csharp', [
     {
         matches: {
             1: 'keyword.new',
-			2: {
+            2: {
                 name: 'support.class',
                 pattern: /\w+/g
             }
@@ -61,27 +61,27 @@ Rainbow.extend('csharp', [
         },
         pattern: /\b(new)\s([^\$].*?)(?=\)|\(|;|&)/g
     },
-	{
-		name: 'string',
-		pattern: /(")(.*?)\1/g
-	},
-	{
-		name: 'integer',
-		pattern: /\b(0x[\da-f]+|\d+)\b/g
-	},
-	{
+    {
+        name: 'string',
+        pattern: /(")(.*?)\1/g
+    },
+    {
+        name: 'integer',
+        pattern: /\b(0x[\da-f]+|\d+)\b/g
+    },
+    {
         name: 'comment',
         pattern: /\/\*[\s\S]*?\*\/|(\/\/)[\s\S]*?$/gm
     },
-	{
-		name: 'operator',
-		// @see http://msdn.microsoft.com/en-us/library/6a71f45d%28v=vs.100%29.aspx
-		// ++ += + -- -= - <<= << <= => >>= >> >= != ! ~ ^ || && &= & ?? :: : *= * |= %= |= == =
-		pattern: /(\+\+|\+=|\+|--|-=|-|&lt;&lt;=|&lt;&lt;|&lt;=|=&gt;|&gt;&gt;=|&gt;&gt;|&gt;=|!=|!|~|\^|\|\||&amp;&amp;|&amp;=|&amp;|\?\?|::|:|\*=|\*|\/=|%=|\|=|==|=)/g
-	},
     {
-		// @see http://msdn.microsoft.com/en-us/library/ed8yd1ha%28v=vs.100%29.aspx
-		name: 'preprocessor',
-		pattern: /(\#if|\#else|\#elif|\#endif|\#define|\#undef|\#warning|\#error|\#line|\#region|\#endregion|\#pragma)[\s\S]*?$/gm
-	}
+        name: 'operator',
+        // @see http://msdn.microsoft.com/en-us/library/6a71f45d%28v=vs.100%29.aspx
+        // ++ += + -- -= - <<= << <= => >>= >> >= != ! ~ ^ || && &= & ?? :: : *= * |= %= |= == =
+        pattern: /(\+\+|\+=|\+|--|-=|-|&lt;&lt;=|&lt;&lt;|&lt;=|=&gt;|&gt;&gt;=|&gt;&gt;|&gt;=|!=|!|~|\^|\|\||&amp;&amp;|&amp;=|&amp;|\?\?|::|:|\*=|\*|\/=|%=|\|=|==|=)/g
+    },
+    {
+        // @see http://msdn.microsoft.com/en-us/library/ed8yd1ha%28v=vs.100%29.aspx
+        name: 'preprocessor',
+        pattern: /(\#if|\#else|\#elif|\#endif|\#define|\#undef|\#warning|\#error|\#line|\#region|\#endregion|\#pragma)[\s\S]*?$/gm
+    }
 ]);
