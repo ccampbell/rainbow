c9a534be41300aaccb7ff7753ba279e8835a3819
Craig Campbell <me@craig.is>
2020-09-10 01:00:20 -0400

    Fix gulp lint command

--- gulpfile.js
+++ gulpfile.js
@@ -158,7 +158,7 @@ gulp.task('test', function(done) {
 });
 
 gulp.task('lint', function() {
-    return gulp.src('src/*.js')
+    gulp.src('src/*.js')
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
